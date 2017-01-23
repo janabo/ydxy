00b59e33d689aa8663d31c3aee903e1118ea99e4
janabo <376921106@qq.com>
2017-01-23 10:52:20 +0800

    学工模块

---
+++ core/src/main/java/com/dk/mp/core/ui/BaseFragment.java
@@ -0,0 +1,46 @@
+package com.dk.mp.core.ui;
+
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 作者：janabo on 2017/1/4 14:27
+ */
+public abstract class BaseFragment extends BaseLazyFragment {
+    protected View mRootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mRootView = inflater.inflate ( getLayoutId ( ), null );
+        return mRootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initialize (view);
+    }
+
+    protected void initialize (View view) {}
+
+    //布局ID
+    protected abstract
+    @LayoutRes
+    int getLayoutId ( );
+
+    @Override
+    public void onFirstUserVisible ( ) {
+        super.onFirstUserVisible ( );
+    }
+
+    @Override
+    public void onUserVisible ( ) {
+        super.onUserVisible ( );
+    }
+}
