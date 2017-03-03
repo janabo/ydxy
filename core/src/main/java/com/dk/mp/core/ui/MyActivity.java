82a68987fe4161dde8f1809bb8d6503371aa1dec
dongqiushu <dongqiushu@163.com>
2017-03-03 17:36:49 +0800

    修改留宿主界面item顺序

--- core/src/main/java/com/dk/mp/core/ui/MyActivity.java
+++ core/src/main/java/com/dk/mp/core/ui/MyActivity.java
@@ -390,4 +390,8 @@ public abstract class MyActivity extends AppCompatActivity{
         ImageView view = (ImageView) findViewById(R.id.brithday);
         view.setVisibility(View.VISIBLE);
     }
+
+    public void CloseDraw(){
+        drawerLayout.closeDrawers();
+    }
 }
