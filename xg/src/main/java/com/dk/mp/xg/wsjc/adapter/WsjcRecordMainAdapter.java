4cb1eb00b073fe902a6bbb9b2317ae014d118c55
janabo <376921106@qq.com>
2017-03-09 11:05:13 +0800

    禅道bug

--- xg/src/main/java/com/dk/mp/xg/wsjc/adapter/WsjcRecordMainAdapter.java
+++ xg/src/main/java/com/dk/mp/xg/wsjc/adapter/WsjcRecordMainAdapter.java
@@ -58,9 +58,11 @@ public class WsjcRecordMainAdapter extends RecyclerView.Adapter<WsjcRecordMainAd
         if(selected == position && type != 3){
             holder.isselect.setVisibility(View.VISIBLE);
             holder.lin_lc.setBackgroundColor(mContext.getResources().getColor(R.color.page_bg));
+            holder.name.setTextColor(mContext.getResources().getColor(R.color.colorPrimary));
         }else{
             holder.isselect.setVisibility(View.INVISIBLE);
             holder.lin_lc.setBackgroundColor(mContext.getResources().getColor(R.color.white));
+            holder.name.setTextColor(mContext.getResources().getColor(R.color.black50));
         }
     }
 
